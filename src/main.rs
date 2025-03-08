d7181b3fedadbee75cc9c36a22561520d7a256f4
Thoth <jonathankurtz@protonmail.com>
2025-03-08 04:01:06 -0600

    removed debug logging, consolidated path construction

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,11 @@ where
     T: Sample,
     U: Sample + hound::Sample + FromSample<T>,
 {
-    let filename = format!("{}.wav", Local::now().format("%Y-%m-%d_%H:%M:%S"));
-    let path = std::env::current_dir().unwrap().join(&filename);
-    println!("{path:?}");
-    std::fs::File::create(path).unwrap();
+    let path = std::env::current_dir().unwrap().join(format!("{}.wav", Local::now().format("%Y-%m-%d_%H:%M:%S")));
+    std::fs::File::create(&path).unwrap();
 
     let spec = WavSpec { channels: config.channels, bits_per_sample: 32, sample_format: hound::SampleFormat::Float, sample_rate: config.sample_rate.0};
-    let mut writer = WavWriter::create(filename, spec).unwrap();
+    let mut writer = WavWriter::create(&path, spec).unwrap();
     for &sample in data {
         writer.write_sample(sample.to_sample::<U>()).ok();
     }
