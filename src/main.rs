54b72ac061ce5c7303e48f32eb520134510ac7b8
thoth@damocles <jonathankurtz@protonmail.com>
2025-03-15 18:17:13 -0500

    implemented custom circular buffer

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,100 @@
 // #![no_main] // todo
 
 use std::{
-    collections::VecDeque, fmt::Debug, fs::File, sync::{mpsc::{self, Receiver, Sender}, Arc, Mutex}, thread
+    fmt::Debug, fs::File, sync::{
+        mpsc::{self, Receiver, Sender}, Arc, Mutex
+    }, thread
 };
 
 use cpal::{
-    traits::{DeviceTrait, HostTrait, StreamTrait}, Sample, FromSample
+    FromSample, Sample,
+    traits::{DeviceTrait, HostTrait, StreamTrait},
 };
 
 use hound::{WavSpec, WavWriter};
 
 use chrono::Local;
 
-fn write_input_data<T, U>(data: &[T], config: &cpal::StreamConfig)
-where
-    T: Sample,
-    U: Sample + hound::Sample + FromSample<T>,
-{
-    let path = std::env::current_dir().unwrap().join(format!("{}.wav", Local::now().format("%Y-%m-%d_%H:%M:%S")));
-    File::create(&path).unwrap();
+#[derive(Default)]
+struct RingBuff<T> {
+    index: usize,
+    saturated: bool,
+    pub capacity: usize,
+    contents: Vec<T>,
+}
 
-    let spec = WavSpec { channels: config.channels, bits_per_sample: 32, sample_format: hound::SampleFormat::Float, sample_rate: config.sample_rate.0};
-    let mut writer = WavWriter::create(&path, spec).unwrap();
-    for &sample in data {
-        writer.write_sample(sample.to_sample::<U>()).ok();
+// impl<'a, T> Iterator<'a, T> for RingBuff<'a, T> {
+    
+    // returns an iterator over the properly arranged items. is no longer correct once something is pushed
+    // fn (&'a self) -> impl Iterator<Item = &'a T> {
+    //     self.contents[..self.index].iter().chain(self.contents[self.index..].iter())
+    // }
+// }
+
+impl<T: Clone + Default> RingBuff<T> {
+    fn new<const CAP: usize>() -> Self {
+        Self {
+            capacity: CAP,
+            contents: { 
+                let mut vec = Vec::<T>::with_capacity(CAP);
+                vec.resize(CAP, T::default());
+                vec
+            },
+            ..Default::default()
+        }
     }
 }
 
-const BUFF_LEN: usize = 2_usize.pow(24);
+impl<T: Clone> RingBuff<T> {
+    fn vectorize(&self) -> Vec<T> {
+        [
+            self.contents[..self.index].iter().clone().map(T::to_owned).collect::<Vec<T>>(),
+            self.saturated.then(|| self.contents[self.index..].iter().clone().map(T::to_owned).collect()).unwrap_or(Vec::new()),
+        ]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<T>>()
+    }
+
+    fn push(&mut self, value: T) {
+        self.contents[self.index] = value;
+        if self.index == self.capacity - 1 {
+            self.index = 0;
+        } else {
+            self.index += 1;
+        }
+    }
+}
+
+fn write_input_data<T, U>(data: &Vec<T>, config: &cpal::StreamConfig)
+where
+    T: Sample<Float = f32>,
+    U: Sample + hound::Sample + FromSample<T>,
+{
+    let path = std::env::current_dir()
+        .unwrap()
+        .join(format!("{}.wav", Local::now().format("%Y-%m-%d_%H:%M:%S")));
+    File::create(&path).unwrap();
+
+    let spec = WavSpec {
+        channels: config.channels,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Float,
+        sample_rate: config.sample_rate.0,
+    };
+    let mut writer = WavWriter::create(&path, spec).unwrap();
+    for &sample in data {
+        writer.write_sample(sample.mul_amp(0.25f32).to_sample::<U>()).ok();
+    }
+}
+
+const BUFF_LEN: usize = 1 << 25;
 
 fn main() {
     let (tx, rx): (Sender<f32>, Receiver<f32>) = mpsc::channel();
 
     //TODO create another channel for signalling termination to child threads
-
-    let buff = Arc::new(Mutex::new(VecDeque::<f32>::with_capacity(BUFF_LEN)));
+    let buff = Arc::new(Mutex::new(RingBuff::<f32>::new::<BUFF_LEN>()));
     let host = cpal::host_from_id(cpal::HostId::Jack).unwrap_or(cpal::default_host());
     let device = host
         .default_input_device()
@@ -51,56 +111,63 @@ fn main() {
         .with_max_sample_rate()
         .config();
 
-
     let buff_clone = buff.clone();
     let config_clone = config.clone();
-    
-    let _read_handle = thread::spawn(move || {
-        let stream = device.build_input_stream(
-            &config_clone,
-            move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                // let mut lock = buff_clone
-                //     .lock()
-                //     .unwrap();
 
-                // lock.append(&mut VecDeque::from_iter(data.to_owned()));
-                // write_input_data::<f32, f32>(data, &&writer_clone);
-                // println!("data!");
-                for point in data {
-                    match tx.send(*point) {
-                        Ok(_) => {},
-                        Err(_) => panic!("Exiting due to send-channel error...")
+    let _read_handle = thread::spawn(move || {
+        let stream = device
+            .build_input_stream(
+                &config_clone,
+                move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                    // let mut lock = buff_clone
+                    //     .lock()
+                    //     .unwrap();
+
+                    // lock.append(&mut VecDeque::from_iter(data.to_owned()));
+                    // write_input_data::<f32, f32>(data, &&writer_clone);
+                    // println!("data!");
+                    for &sample in data {
+                        match tx.send(sample) {
+                            Ok(_) => {}
+                            Err(_) => panic!("Exiting due to send-channel error..."),
+                        }
                     }
-                }
-            },
-            move |err| {
-                eprintln!("An error has been detected during recording: {err:?}");
-            },
-            None,
-        ).unwrap();
+                },
+                move |err| {
+                    eprintln!("An error has been detected during recording: {err:?}");
+                },
+                None,
+            )
+            .unwrap();
         stream.play().expect("Unable to record...");
         thread::park();
     });
 
-    let _write_handle = thread::spawn(move|| {
+    let _write_handle = thread::spawn(move || {
         loop {
             let data = match rx.recv() {
                 Ok(d) => d,
-                Err(_) => { panic!("Exiting due to recv-channel error...") }
+                Err(_) => {
+                    panic!("Exiting due to recv-channel error...")
+                }
             };
 
             let mut lock = buff_clone.lock().unwrap();
-            lock.push_back(data);
+            lock.push(data);
+            // println!("{}", lock.capacity);
         }
     });
 
     let mut line = String::new();
     loop {
         let _ = std::io::stdin().read_line(&mut line);
-        if line == "q" { break }
-        let mut lock = buff.lock().unwrap();
+        if line == "q" {
+            break;
+        }
+
         println!("Saving data...");
-        write_input_data::<f32, f32>(lock.make_contiguous(), &config);
+        let lock = buff.lock().unwrap();
+        write_input_data::<f32, f32>(&lock.vectorize(), &config);
     }
 
     //TODO send signals to children to exit
