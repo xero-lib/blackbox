0fcfa301899b24e23557955347590ed4bbea8946
thoth@damocles <jonathankurtz@protonmail.com>
2025-03-30 21:03:47 -0500

    optimized push_slice and vectorize

--- src/ringbuff.rs
+++ src/ringbuff.rs
@@ -20,32 +20,13 @@ impl<T: Clone + Default, const CAP: usize> RingBuff<T, CAP> {
     }
 }
 
-// could do with some optimization
 impl<T: Clone, const CAP: usize> RingBuff<T, CAP> {
     pub fn vectorize(&self) -> Vec<T> {
-        [
-            self.saturated.then(|| {
-                self.contents[self.index..]
-                    .iter()
-                    .clone()
-                    .map(T::to_owned)
-                    .collect()
-            }),
-            self.index.ne(&0).then(|| {
-                self.contents
-                    .windows(self.index)
-                    .next()
-                    .unwrap_or_default()
-                    .iter()
-                    .clone()
-                    .map(T::to_owned)
-                    .collect::<Vec<T>>()
-            }),
-        ]
-        .into_iter()
-        .flatten()
-        .flatten()
-        .collect::<Vec<T>>()
+        if self.saturated {
+            self.contents[self.index..].iter().cloned().chain(self.contents[..self.index].iter().cloned()).collect()
+        } else {
+            self.contents[..self.index].to_vec()
+        }
     }
 
     // don't you dare commit this
@@ -66,68 +47,16 @@ impl<T: Clone, const CAP: usize> RingBuff<T, CAP> {
         self.increment_index(1);
     }
 
-    // pub fn push_slice(&mut self, values: &[T]) {
-    //     let dist_to_end = (self.capacity - 1) - self.index;
-    //     // does this help?
-    //     // let values_len = values.len();
-
-    //     if values.len() < dist_to_end {
-    //         self.contents.splice(
-    //             self.index..(self.index + values.len()),
-    //             values.iter().map(T::to_owned),
-    //         );
-    //     } else {
-    //         self.contents.splice(
-    //             self.index..(self.capacity - 1), // shouldnt need to be capacity - 1
-    //             values.iter().take(dist_to_end).map(T::to_owned),
-    //         );
-    //         self.contents.splice(
-    //             0..values.len() - dist_to_end,
-    //             values.iter().skip(dist_to_end).map(T::to_owned),
-    //         );
-    //     }
-
-    //     self.increment_index(values.len());
-    // }
-
     pub fn push_slice(&mut self, values: &[T])
-    where
-        T: Copy,
+    where T: Copy
     {
         let dist_to_end = (self.capacity - 1) - self.index;
-        // does this help?
-        // let values_len = values.len();
 
         if values.len() < dist_to_end {
             self.contents[self.index..][..values.len()].copy_from_slice(values);
         } else {
             self.contents[self.index..self.capacity - 1].copy_from_slice(&values[..dist_to_end]);
-            self.contents[0..values.len() - dist_to_end].copy_from_slice(&values[dist_to_end..]);
-        }
-
-        self.increment_index(values.len());
-    }
-
-    // could do with some optimization
-    #[allow(unused)] //only for benchmarks
-    pub fn push_slice_for(&mut self, values: &[T]) {
-        let dist_to_end = (self.capacity - 1) - self.index;
-        // does this help?
-        // let values_len = values.len();
-
-        if values.len() < dist_to_end {
-            for i in self.index..(self.index + values.len()) {
-                self.contents[i] = values[i - self.index].clone();
-            }
-        } else {
-            for i in self.index..(self.capacity) {
-                // shouldnt need to be -1
-                self.contents[i] = values[i - self.index].clone();
-            }
-
-            for i in 0..(values.len() - dist_to_end) {
-                self.contents[i] = values[i + dist_to_end].clone()
-            }
+            self.contents[..values.len() - dist_to_end ].copy_from_slice(&values[dist_to_end..]);
         }
 
         self.increment_index(values.len());
