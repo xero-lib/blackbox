c5dc499ed1ed18db77eb3aa93b53ff47e9707f36
thoth@damocles <jonathankurtz@protonmail.com>
2025-03-30 21:04:07 -0500

    removed defunct benches, consolidated

--- benches/rb_benchmark.rs
+++ benches/rb_benchmark.rs
@@ -5,37 +5,23 @@ use std::random::random;
 
 use blackbox::ringbuff::RingBuff;
 
-fn rb_write(c: &mut Criterion) {
+fn rb_bench(c: &mut Criterion) {
+    let numbers = black_box(
+        (0..1 << 20)
+            .map(|_| random::<i64>() as f32)
+            .collect::<Vec<f32>>(),
+    );
     let mut rb = RingBuff::<f32, { 1 << 20 }>::new();
-    let numbers = (0..1 << 20)
-        .map(|_| random::<i64>() as f32)
-        .collect::<Vec<f32>>();
-
     c.bench_function("rb write 1 << 20", |b| {
         b.iter(|| rb.push_slice(black_box(numbers.as_slice())))
     });
-}
-
-fn rb_write_for(c: &mut Criterion) {
-    let mut rb = RingBuff::<f32, { 1 << 20 }>::new();
-    let numbers = (0..1 << 20)
-        .map(|_| random::<i64>() as f32)
-        .collect::<Vec<f32>>();
-
-    c.bench_function("rb write for 1 << 20", |b| {
-        b.iter(|| rb.push_slice_for(black_box(numbers.as_slice())))
-    });
-}
-
-fn rb_read(c: &mut Criterion) {
-    let mut rb = RingBuff::<f32, { 1 << 20 }>::new();
-    let numbers = (0..1 << 20)
-        .map(|_| random::<i64>() as f32)
-        .collect::<Vec<f32>>();
-    rb.push_slice(black_box(numbers.as_slice()));
-
+    // let mut rb = RingBuff::<f32, {1 << 20}>::new();
+    // c.bench_function("rb write double 1 << 20", |b| b.iter(|| rb.push_slice_double(black_box(numbers.as_slice()))));
+    // let mut rb = RingBuff::<f32, {1 << 20}>::new();
+    // c.bench_function("rb write double closure 1 << 20", |b| b.iter(|| rb.push_slice_double_closure(black_box(numbers.as_slice()))));
+    // let mut rb = RingBuff::<f32, {1 << 20}>::new();
+    // c.bench_function("rb write for 1 << 20", |b| b.iter(|| rb.push_slice_for(black_box(numbers.as_slice()))));
     c.bench_function("rb read 1 << 20", |b| b.iter(|| rb.vectorize()));
 }
-
-criterion_group!(benches, rb_write, rb_write_for, rb_read);
+criterion_group!(benches, rb_bench);
 criterion_main!(benches);
